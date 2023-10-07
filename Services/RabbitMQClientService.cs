a40f792e0705134149df4d7d3bd33c93b68faecb
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-10-07 19:11:20 +0300

    Committed

--- Services/RabbitMQClientService.cs
+++ Services/RabbitMQClientService.cs
@@ -1,6 +1,18 @@
-﻿namespace Watermark.Services
+﻿using RabbitMQ.Client;
+
+namespace Watermark.Services
 {
     public class RabbitMQClientService
     {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public static string ExchangeName = "ImageDirectExchange";
+        public static string RoutingWatermak = "watermark-route-image";
+        public static string QueueName = "queue-watermark-image";
+
+        private readonly ILogger<RabbitMQClientService> _logger;
+
     }
 }
