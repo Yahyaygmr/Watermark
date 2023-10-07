0cc4b2fb87b8eca61b74409be306e9f018b70a0f
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-10-07 19:57:31 +0300

    Committed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 using Watermark.Models;
+using Watermark.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton(sp =>
+{
+    IConfiguration configuration = sp.GetRequiredService<IConfiguration>();
+    string rabbitMqUri = configuration.GetConnectionString("RabbitMQ"); // "RabbitMQ" burada baðlantý dizesi adýnýza göre ayarladýðýnýz isimdir.
+
+    return new ConnectionFactory() { Uri = new Uri(rabbitMqUri) };
+});
+builder.Services.AddSingleton<RabbitMQClientService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
--- Services/RabbitMQClientService.cs
+++ Services/RabbitMQClientService.cs
@@ -2,7 +2,7 @@
 
 namespace Watermark.Services
 {
-    public class RabbitMQClientService
+    public class RabbitMQClientService: IDisposable
     {
         private readonly ConnectionFactory _connectionFactory;
         private IConnection _connection;
@@ -14,5 +14,42 @@ namespace Watermark.Services
 
         private readonly ILogger<RabbitMQClientService> _logger;
 
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger, IModel channel)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+            _channel = channel;
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+
+            if (_channel is { IsOpen: true })
+            {
+                return _channel;
+            }
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(ExchangeName, type: "direct", true, false);
+            _channel.QueueDeclare(QueueName, true, false, false, null);
+
+            _channel.QueueBind(exchange: ExchangeName, queue: QueueName, routingKey: RoutingWatermak);
+            _logger.LogInformation("RabbitMQ ile Bağlantı Kuruldu...");
+
+            return _channel;
+
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel.Dispose();
+
+            _connection?.Close();
+            _connection?.Dispose();
+
+            _logger.LogInformation("RabbitMQ Bağlantısı Kapandı...");
+
+        }
     }
 }
